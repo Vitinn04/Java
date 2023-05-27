ec909076388d15e3dd59b3e6d08edc29d752d30a
VITINN <103295994+Vitinn04@users.noreply.github.com>
2023-05-26 22:29:30 -0300

    Add files via upload

---
+++ Tabuada3.java
@@ -0,0 +1,12 @@
+package lacos;
+
+public class Tabuada3 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i < 11; i++) {
+			System.out.println(3*i);
+		}
+
+	}
+
+}
---
+++ Valoresinteriros.java
@@ -0,0 +1,13 @@
+package lacos;
+
+public class Valoresinteriros {
+
+	public static void main(String[] args) {
+		for (int i = 10; i < 101; i++) {
+			System.out.println(i);
+			
+		}
+
+	}
+
+}
