e69ccb3f69798d0868bd3769360af8b12d68916c
VITINN <103295994+Vitinn04@users.noreply.github.com>
2023-06-01 15:10:53 -0300

    Update TesteMesa.java

--- TesteMesa.java
+++ TesteMesa.java
@@ -4,11 +4,11 @@ public class TesteMesa {
 
 	public static void main(String[] args) {
 		int n=0;for (int i = 3; i<7;i++) {n = n + i;}
-		System.out.println(n);
+		
 		
 		//N = 0 | N = 3 | N = 7 | N = 12 | N = 18
 		//N = 3 | I = 4 | I = 5 | I = 6  | I = 6
 
 	}
-
+	System.out.println(n);
 }
